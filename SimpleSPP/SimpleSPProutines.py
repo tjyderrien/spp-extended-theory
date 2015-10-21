d557509543d112a5bbe0c386f16a4871e80673b7
Thibault Derrien <derrien@fzu.cz>
2015-10-21 19:15:56 +0200

    SimpleSPP: adding period plotting

--- SimpleSPP/SimpleSPProutines.py
+++ SimpleSPP/SimpleSPProutines.py
@@ -217,8 +217,9 @@ def SPPactiveInterfaces(dbarray, comment):
         if(ExperimentalAchievable and (SPPperiod!=0)):
           if (np.mod(counter, 20) == 0): 
             #show the table line each 20 lines
-            print '{0:12s} {1:12s} {2:15s} {3:12s} {4:12s} {5:11s} {6:16s} {7:16s} {8:12s} {9:19s} {10:19s} {11:15s}'.format("# Substrate", "Layer", "Wavelength (nm)", "OldSPPactive", "NewSPPactive", "Period (nm)", "DecayDepth1 (nm)", "DecayDepth2 (nm)", "Reflectivity", "OpticalPenetration1", "OpticalPenetration2", "DecayLength")
-          
+            if (comment):
+							print '{0:12s} {1:12s} {2:15s} {3:12s} {4:12s} {5:11s} {6:16s} {7:16s} {8:12s} {9:19s} {10:19s} {11:15s}'.format("# Substrate", "Layer", "Wavelength (nm)", "OldSPPactive", "NewSPPactive", "Period (nm)", "DecayDepth1 (nm)", "DecayDepth2 (nm)", "Reflectivity", "OpticalPenetration1", "OpticalPenetration2", "DecayLength")
+
           counter=counter+1
           print '{0:12s} {1:12s} {2:15f} {3:12s} {4:12s} {5:11f} {6:16f} {7:16f} {8:12f}  {9:19f} {10:19f} {11:15f}'.format(Material1, Material2, Wavelength, OldSPPactiveBool, NewSPPactiveBool, SPPperiod, SPPdecayDepth1, SPPdecayDepth2, Reflectivity, OpticalPenetration1, OpticalPenetration2, SPPdecayLength)
         
--- SimpleSPP/makeTable.py
+++ SimpleSPP/makeTable.py
@@ -3,13 +3,14 @@
 
 # IMPORT LIBRARIES
 from SimpleSPProutines import *
+from plotGraph import *
 
 # PHYSICAL INPUT
-wavelength = 800e-9
-epsAir=1e0
-epsSi0=13.64+0.048j
-meffe=0.18
-nuSi=(1.1e-15)**-1
+#wavelength = 800e-9
+#epsAir=1e0
+#epsSi0=13.64+0.048j
+#meffe=0.18
+#nuSi=(1.1e-15)**-1
 
 #example=period(betaSPP(wavelength, epsAir, Drude(wavelength, 1e28, epsSi0, nuSi)))
 
@@ -32,8 +33,6 @@ database="MaterialOpticalDatabaseForPlasmonics.csv"
 # Build database array for choosing which material can be of interest to irradiate
 dbarray = loadtxt(database, dtype='str', delimiter='\t')
 
-#f = open('SPPactiveInterfaces.dat', 'w+')
-
 # To calculate symmetric SPP compatible interfaces, use the following line
 SPPactiveInterfacesArray = SPPactiveInterfaces(dbarray, '')
 
@@ -43,9 +42,29 @@ SPPactiveInterfacesArray = SPPactiveInterfaces(dbarray, '')
 # To calculate asymmetric NEGATIVE SPP compatible interfaces, use the following line
 #SPPactiveInterfacesArray = AsymmetricSPPnegActiveInterfaces(dbarray, '')
 
-#f.write(SPPactiveInterfacesArray)
-
 """ TODO: interface this with HTML for publication on the web. 
 1. Put results into a NP.array.
 2. Use a converter to HTML, CSV and PDF maybe. 
 """
+
+# Plotting the LIPSS period as function of materials
+
+# Irradiation in air environment
+# Concept: In the generated file with SPP active interfaces, plot the period as function of something
+
+# SPPactiveInterfacesArray contains all data we need, just remove lines starting with #. 
+
+MaterialToSelect="Air"
+
+plt.figure()
+plt.xlabel('SPP decay length')
+plt.ylabel('SPP period (nm)')
+
+for i in SPPactiveInterfacesArray:
+	if (SPPactiveInterfacesArray[i,0]==MaterialToSelect): #select only entries which corresponds to interface of interest
+		# Construct the interesting data point with x: decay length, y: period, txt: name of interface
+		#TODO x = append()
+		
+plt.plot(,y,label='')
+		
+

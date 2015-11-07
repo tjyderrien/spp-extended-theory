5be03dc0376b710c000e0f7275b7cb32c2292f9e
Thibault Derrien <derrien@fzu.cz>
2015-11-07 18:18:43 +0100

    LIPSS database: general structure and table optimization to CSV (not finished)

---
+++ LIPSS-database/Structure of LIPSS database.mm
@@ -0,0 +1,143 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1446915353546" ID="ID_72720721" MODIFIED="1446915640992" TEXT="Structure of LIPSS database">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1446915479297" ID="ID_1394241196" MODIFIED="1446915640979" POSITION="right" TEXT="Author/origin">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1446915492535" ID="ID_1569819012" MODIFIED="1446915640980" TEXT="Author">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915496866" ID="ID_1375064101" MODIFIED="1446915640980" TEXT="Journal">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915498695" ID="ID_36165561" MODIFIED="1446915640981" TEXT="Year">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915503316" ID="ID_730209346" MODIFIED="1446915640982" TEXT="Path (to access more data)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1446915360856" ID="ID_849333805" MODIFIED="1446915640982" POSITION="right" TEXT="Laser parameters">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1446915459021" ID="ID_878825282" MODIFIED="1446915640983" TEXT="Laser wavelength">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915461801" ID="ID_358500692" MODIFIED="1446915640983" TEXT="Pulse duration">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915463574" ID="ID_212488312" MODIFIED="1446915640984" TEXT="Laser peak fluence">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915470058" ID="ID_362875836" MODIFIED="1446915640984" TEXT="Laser energy">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915520600" ID="ID_1667945579" MODIFIED="1446915640985" TEXT="Number of pulses">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915523355" ID="ID_423648208" MODIFIED="1446915640985" TEXT="Angle of incidence">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915526372" ID="ID_145324717" MODIFIED="1446915640985" TEXT="Polarization">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446916664880" ID="ID_401428030" MODIFIED="1446916665940" TEXT="Linear">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1446916666168" ID="ID_1399576379" MODIFIED="1446916667228" TEXT="Circular">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1446915535820" ID="ID_1373108132" MODIFIED="1446915640986" TEXT="Scanning velocity">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446916629722" ID="ID_80005709" MODIFIED="1446916632138" TEXT="Repetition rate">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446916650748" ID="ID_1341202585" MODIFIED="1446916653338" TEXT="Spot diameter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446916673205" ID="ID_1378761692" MODIFIED="1446916679626" TEXT="Focal of last lens">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1446915367186" ID="ID_1423363475" MODIFIED="1446915640986" POSITION="right" TEXT="Material parameters">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1446915432042" ID="ID_189921049" MODIFIED="1446915640987" TEXT="Single bulk sample">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446916535880" ID="ID_489712886" MODIFIED="1446916543171" TEXT="Bulk material&apos;s">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1446916548131" ID="ID_1728834956" MODIFIED="1446916551329" TEXT="Dielectric permittivity"/>
+<node COLOR="#111111" CREATED="1446916598638" ID="ID_949126944" MODIFIED="1446916612063" TEXT="Melting threshold fluence at this pulse duration"/>
+<node COLOR="#111111" CREATED="1446916615064" ID="ID_237731262" MODIFIED="1446916619767" TEXT="Ablation threshold for this pulse duration"/>
+<node COLOR="#111111" CREATED="1446916639192" ID="ID_1701450204" MODIFIED="1446916644855" TEXT="Dopant density"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1446915445031" ID="ID_1381977297" MODIFIED="1446915640987" TEXT="Layered sample">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446915560752" ID="ID_47547299" MODIFIED="1446915640988" TEXT="Number of layers">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1446915564397" ID="ID_1135469155" MODIFIED="1446915640988" TEXT="Materials involved">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1446915613556" ID="ID_1659501512" MODIFIED="1446915640988" TEXT="Thickness of layers">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1446915370022" ID="ID_69374171" MODIFIED="1446915640989" POSITION="right" TEXT="Environment parameters">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1446915420239" ID="ID_975746180" MODIFIED="1446915640989" TEXT="Pressure">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915422536" ID="ID_1167302889" MODIFIED="1446915640990" TEXT="Surrounding atmosphere">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1446915376669" ID="ID_1930608884" MODIFIED="1446915640990" POSITION="right" TEXT="Measurements">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1446915390231" ID="ID_1232179053" MODIFIED="1446915640990" TEXT="Period">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1446915394884" ID="ID_1713354660" MODIFIED="1446915640991" TEXT="Fourier analysis">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1446915397282" ID="ID_1376144139" MODIFIED="1446915640991" TEXT="Direct measurement">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1446915403251" ID="ID_1604357051" MODIFIED="1446915640991" TEXT="Regularity">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1446915406657" ID="ID_1580811863" MODIFIED="1446915640992" TEXT="Orientation compared to laser polarization">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>

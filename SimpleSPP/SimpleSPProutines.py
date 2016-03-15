cc835f560c67320eb468d77c78cec9703e3652f3
Thibault Derrien <derrien@fzu.cz>
2016-03-15 02:16:34 +0100

    Updated figures for Iaroslav.

--- SimpleSPP/SimpleSPProutines.py
+++ SimpleSPP/SimpleSPProutines.py
@@ -475,11 +475,11 @@ def SPPactiveInterfaces(dbarray, comment):#{{{
         
         # Print only the experimentally possible cases: SPP active depth must be smaller than absorption depth. 
         # ensure that SPPdecayDepth is smaller than layer thickness, to avoid shift of dispersion relation
-        ExperimentalAchievable = ExperimentallyAchievable(OpticalPenetration1, SPPdecayDepth1)
+        ExperimentalAchievable = True #ExperimentallyAchievable(OpticalPenetration1, SPPdecayDepth1)
         
         # Print the table of active SPP interfaces for all cases or only new SPP interfaces					
         #if( not (comment=="new")): 
-        if(ExperimentalAchievable or (SPPperiod!=0)):
+        if(ExperimentalAchievable and (SPPperiod!=0) and (SPPdecayLength < 10000e0) and (abs(eps2.real) < eps2.imag)):
           counter=counter+1
           #print SPParray.shape
           SPParray = np.vstack((SPParray, [Material1, Material2, Wavelength, OldSPPactiveBool, NewSPPactiveBool, 

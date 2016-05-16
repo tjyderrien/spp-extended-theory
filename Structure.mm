fb7df0811426aa1b76ddc9e1c0386f9233a3e135
Thibault Derrien <derrien@fzu.cz>
2016-05-17 01:42:14 +0200

    Added a tree with structure of the functions into folders and files.

---
+++ Structure.mm
@@ -0,0 +1,208 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1463059249318" ID="ID_296625567" MODIFIED="1463059349809" TEXT="SPP-extended theory">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1463059254124" ID="ID_1087957046" MODIFIED="1463059349806" POSITION="right" TEXT="SimpleSPP">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1463059371756" ID="ID_1420743672" MODIFIED="1463059372702" TEXT="plotSPPmultiMaterialData.py">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1463420508274" ID="ID_398056199" MODIFIED="1463420748407" TEXT="CleanStrArray(Material)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420516920" ID="ID_1883092104" MODIFIED="1463420517391" TEXT="plotDatabaseMaterials(database, legend, outputfile, query)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420526515" ID="ID_1085427159" MODIFIED="1463420526922" TEXT="plotDatabasePeriod(database, legend, outputfile, query, metal)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420547482" ID="ID_1619006063" MODIFIED="1463420548098" TEXT="plotSeveralWavelengths(database1, database2, reverse, metal)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1463420655209" ID="ID_1921752155" MODIFIED="1463421055344" TEXT="advancedPlotting.py">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1463420664882" ID="ID_1496127505" MODIFIED="1463420665508" TEXT="get_text_positions(x_data, y_data, txt_width, txt_height)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420671436" ID="ID_1686916978" MODIFIED="1463420671934" TEXT="text_plotter(x_data, y_data, text_content, text_positions, axis,txt_width,txt_height, color)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420677218" ID="ID_1557792705" MODIFIED="1463420677618" TEXT="makePlot(x_data, y_data, tags, filename, plottitle, labelx, labely, functionlabel, textcolor)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1463420688678" ID="ID_1355072545" MODIFIED="1463420691674" TEXT="importPalikData.py">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1463420701085" ID="ID_1108531821" MODIFIED="1463420703774" TEXT="importFromPalikGraph(wavelength, folder, filename, plotting=1)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420716336" ID="ID_1654226274" MODIFIED="1463420716899" TEXT="importFromEpsGraph(wavelength, folder, filename, plotting)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420725388" ID="ID_888306573" MODIFIED="1463420725951" TEXT="importFromAbsorptionData(wavelength, folder, filename, plotting)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463420733224" ID="ID_1684343791" MODIFIED="1463420733585" TEXT="importFromTables(wavelength, folder, filename, plotting)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1463059380289" ID="ID_1828513528" MODIFIED="1463059380958" TEXT="plotMultiwavelength.py">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1463420925204" ID="ID_1743780999" MODIFIED="1463420927977" TEXT="plotSipe.py">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1463420863279" ID="ID_813261292" MODIFIED="1463420867956" TEXT="all math functions used by Sipe">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1463420822310" ID="ID_70732454" MODIFIED="1463420824292" TEXT="makeTable.py">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1463059399061" ID="ID_1250602551" MODIFIED="1463059400041" TEXT="SimpleSPProutines.py">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1463059537791" ID="ID_1425047160" MODIFIED="1463059538140" TEXT="ExportToTxt(dbarray, filename)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1463419319946" ID="ID_1524730059" MODIFIED="1463419469947" TEXT="SPP condition">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1463059429171" ID="ID_9800461" MODIFIED="1463419370417" TEXT="AsymmetricSPPconditionPos(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059432923" ID="ID_1937064885" MODIFIED="1463419370419" TEXT="AsymmetricSPPconditionNeg(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059524980" ID="ID_1335861264" MODIFIED="1463419331057" TEXT="AsymmetricSPPposActiveInterfaces(dbarray, comment)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059529576" ID="ID_1081482450" MODIFIED="1463419332647" TEXT="AsymmetricSPPnegActiveInterfaces">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059515398" ID="ID_1804321591" MODIFIED="1463419336453" TEXT="ExperimentallyAchievable(OPD, DecayDepth)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059441476" ID="ID_1744791706" MODIFIED="1463419378858" TEXT="SPPconditionValue(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059447688" ID="ID_419423776" MODIFIED="1463419378860" TEXT="SPPcondition(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059452537" ID="ID_479889396" MODIFIED="1463419412620" TEXT="OldSPPcondition(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059520004" ID="ID_155805972" MODIFIED="1463419334099" TEXT="SPPactiveInterfaces(dbarray, comment)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1463419471349" ID="ID_1054827252" MODIFIED="1463419473101" TEXT="SPP properties">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1463059424008" ID="ID_1700060851" MODIFIED="1463419405872" TEXT="betaSPP(wavelength, eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059466982" ID="ID_882852788" MODIFIED="1463419349644" TEXT="DecayDepth(kzSPP)">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1463421654839" ID="ID_1846738469" MODIFIED="1463421657776" TEXT="Return distance"/>
+</node>
+<node COLOR="#111111" CREATED="1463059473206" ID="ID_699901404" MODIFIED="1463419347671" TEXT="kzSPP(wavelength, eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1463421649392" ID="ID_1361326468" MODIFIED="1463421653221" TEXT="Returns kz complex"/>
+</node>
+<node COLOR="#111111" CREATED="1463059479976" ID="ID_39111654" MODIFIED="1463419345731" TEXT="DecayLengthSPP(beta)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059462048" ID="ID_1907013216" MODIFIED="1463419420603" TEXT="period(beta)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059485436" ID="ID_960970793" MODIFIED="1463419344095" TEXT="SPPlength(beta)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059491780" ID="ID_86210483" MODIFIED="1463419340584" TEXT="LifeTimeRaether(beta, eps2, eps1)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059495228" ID="ID_1075141978" MODIFIED="1463419342025" TEXT="LifetimeDerrien(beta, vg)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463421597404" ID="ID_267063361" MODIFIED="1463421598177" TEXT="deltaBetaSPP(wavelength, eps1, eps2, deps1r, deps1c, deps2r, deps2c)"/>
+<node COLOR="#111111" CREATED="1463421605630" ID="ID_757009886" MODIFIED="1463421606221" TEXT="deltaPeriodSPP(wavelength, eps1, eps2, deps1r, deps1c, deps2r, deps2c)"/>
+</node>
+<node COLOR="#990000" CREATED="1463419383405" ID="ID_1432429344" MODIFIED="1463419391273" TEXT="Spectrometry">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1463059413077" ID="ID_1082732867" MODIFIED="1463419396205" TEXT="reflectivity(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059508421" ID="ID_131414437" MODIFIED="1463419402782" TEXT="Drude(wavelength, ne, epsilon, nu)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059417358" ID="ID_1315189718" MODIFIED="1463419415220" TEXT="EpsilonToIndex(eps)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463421716883" ID="ID_1117153755" MODIFIED="1463421727070" TEXT="IndexToEpsilon(n)"/>
+<node COLOR="#111111" CREATED="1463059408230" ID="ID_902146071" MODIFIED="1463419417582" TEXT="omega(wavelength)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463059499688" ID="ID_878117160" MODIFIED="1463419410159" TEXT="EffectiveIndex(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1463419427399" ID="ID_1927015400" MODIFIED="1463419428355" TEXT="Maths">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1463059544146" ID="ID_1742515960" MODIFIED="1463419437175" TEXT="RealDerivativeByComplex(f,z)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1463420535825" ID="ID_497520133" MODIFIED="1463421038090" TEXT="Swap(eps1, eps2)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1463421499229" ID="ID_1415100012" MODIFIED="1463421501432" TEXT="Database management">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1463059533636" ID="ID_144908283" MODIFIED="1463419400318" TEXT="GenerateDatabase()">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1463422637205" ID="ID_573998686" MODIFIED="1463422656582" TEXT="Generates SPP active material couple data, using MaterialDatabase.csv."/>
+</node>
+<node COLOR="#111111" CREATED="1463421755879" ID="ID_1921108736" MODIFIED="1463421756252" TEXT="FilterDatabaseLowerThan(SPPdb, query, FieldIndex)"/>
+<node COLOR="#111111" CREATED="1463421745385" ID="ID_449093487" MODIFIED="1463421745386" TEXT="FilterDatabase(SPPdb, query, FieldIndex)">
+<node COLOR="#111111" CREATED="1463422610947" ID="ID_783914668" MODIFIED="1463422629131" TEXT="Reduce number of elements matching with query in FieldIndex"/>
+</node>
+<node COLOR="#111111" CREATED="1463421794138" ID="ID_1489462821" MODIFIED="1463421794544" TEXT="ExtractDataDb(SPPdbFiltered)">
+<node COLOR="#111111" CREATED="1463422230737" ID="ID_1362119908" MODIFIED="1463422244822" TEXT="From MxN tuple, generate N vectors. "/>
+</node>
+<node COLOR="#111111" CREATED="1463421763844" ID="ID_1686233269" MODIFIED="1463421764296" TEXT="ExtractMaterialData(Database)">
+<node COLOR="#111111" CREATED="1463422668868" ID="ID_747361046" MODIFIED="1463422678945" TEXT="From MaterialDatabase.csv, extract the matching data"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1463420970119" ID="ID_1078524174" MODIFIED="1463420972893" POSITION="right" TEXT="Non-linear refraction">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1463420973489" ID="ID_1515153131" MODIFIED="1463420992570" TEXT="Numerical data to be generalized in Python">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1463059263885" ID="ID_610763149" MODIFIED="1463059349807" POSITION="right" TEXT="MultilayerSPP">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1463059268889" ID="ID_361534380" MODIFIED="1463059349807" POSITION="left" TEXT="Databases">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1463059272065" ID="ID_1065234883" MODIFIED="1463059349808" TEXT="DISCRETE: Few wavelengths, many materials">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1463059283691" ID="ID_1375589915" MODIFIED="1463421827245" TEXT="CONTINUOUS: Continuous wavelengths, many materials">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>

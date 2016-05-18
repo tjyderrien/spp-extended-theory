af88bff079185dac06c72985593c8f4b99d29c2e
Thibault Derrien <derrien@fzu.cz>
2016-05-19 01:23:24 +0200

    SPP-ext: updated script for automatic comparison with experiments

--- SimpleSPP/Results/Air/Iaroslav/1030nm/plot.py
+++ SimpleSPP/Results/Air/Iaroslav/1030nm/plot.py
@@ -113,7 +113,7 @@ print "Final list: "+str(DispersionAngle)+", "+str(Lspp)
 print ""
 print "| Plotting the results..."
 plt.errorbar(1E6*Lspp, DispersionAngle, yerr=DispersionAngleError, fmt='o')
-ax.set_xscale('log')
-ax.set_yscale('log')
+#ax.set_xscale('log')
+#ax.set_yscale('log')
 #plt.show()
 plt.savefig('OriginOfRegularity.eps')

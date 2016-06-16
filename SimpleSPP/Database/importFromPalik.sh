4afa31e95e0b3a2e4e15e037394e855ef1e47860
Thibault Derrien <derrien@fzu.cz>
2016-06-16 17:37:39 +0200

    Fixed Database/importFromPalik.sh and added Be-Palik

--- SimpleSPP/Database/importFromPalik.sh
+++ SimpleSPP/Database/importFromPalik.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [ -e $1 ]; then
+set -e
+
+material=$1
+
+if [ -z ${material} ]; then
 	echo "Usage: ./importFromPalik.sh <Material symbol to include (Si, Be, ...) >"
 	exit
 else 
@@ -9,39 +13,50 @@ fi
 
 # exit
 
-echo "Searching for $1 in available files..."
-
-search () {
-	if [ -e $1 ]; then
-		find | grep "${research}"
-	else
-		echo "[Search] No argument was given."
-		exit
-	fi
+searchString () {
+#   if [ -n $1 ]; then
+  find | grep "$1" -i
+#   else
+#     exit
+#   fi
 }
 
-searchnum=$( search "$1" | wc -l )
+echo "Searching for ${material} in available files..."
+search=$( searchString "${material}")
+searchnum=$( searchString "${material}" | wc -l )
 echo "${searchnum} result(s) found."
+echo "${search}" 
 
 if [ ${searchnum} -ge 2  ]; then
-	echo "** Search: Can you be more specific ?"
-	echo "** Results"
-	search "$1"
-	exit
+  echo "** Search: Can you be more specific ?"
+  echo "** Results"
+  echo "${search}"
+  exit
+else
+  searchMod=$( echo ${search} | tr "_" " " | tr "/" " " | tr '.' ' ' )
+  material=$( echo ${searchMod} | awk '{ print $2 }' )
+  searchPath=$( echo ${search} | tr "/" " " )
+  datafile=$( echo ${searchPath} | awk '{ print $3 }' )
+  source="Palik"
 fi
 
-echo "Creating headers..."
-cat << EOF > ${1}_Palik.1
+filename="${material}-${source}"
+
+echo "Build file ${filename}..."
+echo "[Building] Headers..."
+cat << EOF > ${filename}.1
 # Optical constants for $1
 # 'Handbook of Optical Constants of Solids', Ed. by Edward D. Palik,
 # Academic Press, Inc., 1985.
 # Lambda (A) n k
 EOF
 
-echo "Stacking header and n,k file..."
-cat "${1}_Palik.1" Palik/$1_palik.nk > ${1}_Palik
+echo "[Building] Stacking header and n,k file..."
+cat ${filename}.1 Palik/${datafile} > ${filename}
 
-echo "Cleaning..."
-rm ${1}_Palik.1
+echo "[Cleaning]"
+rm ${filename}.1
+
+ls -lhtr ${filename}
 
 echo "Done."

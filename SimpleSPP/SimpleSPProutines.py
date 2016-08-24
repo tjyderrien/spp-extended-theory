Fixed formula coefficients to obtain same esult for Ag.

SimpleSPP/SimpleSPProutines.py
@@ -22,7 +22,7 @@ eta = 5e0 #assumed precision error on the dielectric permittivity
 # Settings for matplotlib
 #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'], 'size':'16'})
 ## for Palatino and other serif fonts use:
-rc('font', **{'family':'serif', 'serif':['Palatino'], 'size':'20'})
+rc('font', **{'family':'serif', 'serif':['Palatino'], 'size':'18'})
 rc('text', usetex=True)
 mp.rcParams['legend.numpoints'] = 1
 
@@ -698,7 +698,7 @@ def HohenauGroupVelocity(omega, k):
   # (<array of omegas>, array of k) -> <array of group velocities>
   # 
   #vg = c/(nspp - wavelength * dnspp / dwavelength)
-  vg = np.diff(omega) / np.diff(k)
+  vg = np.diff(omega) / np.diff(k.real)
   return vg
 
 def RealDerivativeByComplex(f,z):
@@ -714,8 +714,9 @@ def RealDerivativeByComplex(f,z):
 
 def LifeTimeRaether(beta, eps2, eps1):
 	omegasppimag=beta.real * c * eps1.imag/(2e0*eps1.real**2) * (eps1.real * eps2.real)/(eps1.real + eps2.real)
-	lifetime=2e0*pi/omegasppimag #Raether formula
-	#lifetime=1e0/omegasppimag #modified Raether formula to match with complex group velocity approach
+	#lifetime=2e0*pi/omegasppimag #Raether original formula
+	#lifetime=2e0/omegasppimag #Raether modified formula, we just dropped the pi. 
+	lifetime=0.5e0/omegasppimag #modified Raether formula to match with complex group velocity approach
 	return lifetime
 
 def SPPlength(beta): #{{{

SimpleSPP/plotMultiwavelength.py
@@ -55,7 +55,7 @@ try:
     ShowPictures = True
 except: 
   print "** Warning: No-show command was not defined."
-  print sys.argv[4]
+  ShowPictures = True
     
 
 MaterialFile2 = query+source
@@ -263,14 +263,21 @@ plt.xlabel('Wavelength $\lambda$ (nm)')
 plt.ylabel(r'SPP lifetime $\tau_{\mbox{SPP}}$ (ps)')
 plt.plot(1e9*2*pi*c/omegaspp[1:], 1E12*LifeTimeOld, 'b-', label=r'Raether, $\omega \in \mathbb{C}$, $\beta \in \mathbb{R}$')
 plt.plot(1e9*2*pi*c/omegaspp[1:], 1E12*LifeTimeRe, 'k-', label=r'$\tau=L_{SPP}/v_g$, PMA, $\omega \in \mathbb{R}$, $\beta \in \mathbb{R}$')
-plt.plot(1e9*2*pi*c/omegaspp[1:], 1E12*LifeTimeNew, 'r--', label=r'$\tau=L_{SPP}/v_g$, Wirtinger formula, $\omega \in \mathbb{R}$, $\beta \in \mathbb{C}$')
+plt.plot(1e9*2*pi*c/omegaspp[1:], 1E12*LifeTimeNew, 'r--', label=r'$\tau=L_{SPP}/v_g$, Wirtinger, $\omega \in \mathbb{R}$, $\beta \in \mathbb{C}$')
 #plt.plot(1e9*2*pi*c/omegaspp[1:], 1E12*LifeTimePhase, 'r--', label=r'Phase, real $\omega$, complex $\beta$')
 #plt.plot(1e9*2*pi*c/omegaspp, 1E12*LifeTimeApprox, 'b--', label=r'Phase, approx.')
 #plt.title(MaterialFile1+'/'+MaterialFile2+' interface')
 plt.legend(loc=2)
 #plt.xticks(np.arange(0, 2500, 500))
-#plt.axis([300,2000,0,0.1])
+if(query == "Ti"):
+  maxLifeTime=0.1
+elif (query == "Ag"):
+  maxLifeTime=10
+else:
+  maxLifeTime=10
+plt.axis([300,2000,0,maxLifeTime])
 plt.savefig(MaterialFile1+MaterialFile2+'Lifetime.eps')
+plt.savefig(MaterialFile1+MaterialFile2+'Lifetime.png')
 #plt.show()
 
 
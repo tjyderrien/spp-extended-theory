64bcd44bd52c11a06316c3273e6c6b7c98d63563
Thibault Derrien <derrien@fzu.cz>
2016-11-16 00:38:34 +0100

    libKeldysh > Added a default parameter to dielectric permittivity for Intensity calculation.

--- Keldysh/libKeldysh.py
+++ Keldysh/libKeldysh.py
@@ -87,24 +87,48 @@ def KeldyshFunction(Keldysh1, Keldysh2, Ueff, nmax, wavelength): #{{{
 """
 #}}}
 
+## Corrected Keldysh function according to Vitaly Gruzdev (see Ref in details). 
+# Kane direct band gap structure
+# This function is corrected according to Gruzdev, Opt. Eng. 53, 122515 (2014)
+# Only factors of 2 are removed from inside the Keldysh integral, but inserted 
+# as a proportion in IonizationRate_Gruzdev function. 
 def KeldyshFunction_Gruzdev(Keldysh1, Keldysh2, Ueff, nmax, wavelength): #{{{
   omegaLaser = 2.*pi*c/wavelength #SI
   n=np.arange(0,nmax+1)
   #print n
-  sumtable=np.exp(-pi*n*(ellipk(Keldysh1**2)-ellipe(Keldysh1**2))/ellipe(Keldysh2**2))*DawsonIntegral(pi*np.sqrt( ((2.0*np.trunc(Ueff/hbar/omegaLaser+1.))-2.0*Ueff/hbar/omegaLaser + n) / (2.0 * ellipk(Keldysh2**2)*ellipe(Keldysh2**2)) ) )
+  sumtable=np.exp(-pi*n*(ellipk(Keldysh1**2)-ellipe(Keldysh1**2))/ellipe(Keldysh2**2))*DawsonIntegral(pi*np.sqrt( ((np.trunc(Ueff/hbar/omegaLaser+1.))-Ueff/hbar/omegaLaser + n) / (2.0 * ellipk(Keldysh2**2)*ellipe(Keldysh2**2)) ) )
   #print "Effective gap: "+str(Ueff/e)+" eV."
   #print sumtable
-  result = np.multiply(np.sqrt(pi/(2.*ellipk(Keldysh2**2))),np.sum(sumtable))
+  result = np.multiply(np.sqrt(pi/(2.*ellipk(Keldysh2**2))), np.sum(sumtable))
   return result
 
+## The original Keldysh function for Kane direct band gap
+# This function is known to contain mistakes. 
 def IonizationRate(Keldysh1, Keldysh2, KeldyshFunctionResult, Ueff, wavelength):
   omegaLaser = 2.*pi*c/wavelength
   IonizationRate=2.*omegaLaser/(9.*pi)*((omegaLaser*m_e)/(hbar*Keldysh1))**(1.5)*KeldyshFunctionResult*np.exp(-pi*np.trunc(Ueff/hbar/omegaLaser+1)*((ellipk(Keldysh1**2)-ellipe(Keldysh1**2))/(ellipe(Keldysh2**2))))
-	  
+  
   return IonizationRate
 
-def FieldToIntensity(Field):
-  intensity = 0.5 * c * epsilon_0 * Field**2
+## Corrected Keldysh photoionization probability according to Vitaly Gruzdev (see Ref in details). 
+# Kane direct band gap structure
+# This function is corrected according to Gruzdev, Opt. Eng. 53, 122515 (2014)
+# Only factor of 2 was added outside the Keldysh integral, but removed
+# inside. 
+def IonizationRate_Gruzdev(Keldysh1, Keldysh2, KeldyshFunctionResult, Ueff, wavelength):
+  omegaLaser = 2.*pi*c/wavelength
+  IonizationRate=2. * IonizationRate(Keldysh1, Keldysh2, KeldyshFunctionResult, Ueff, wavelength)
+  #2.*omegaLaser/(9.*pi)*((omegaLaser*m_e)/(hbar*Keldysh1))**(1.5)*KeldyshFunctionResult*np.exp(-pi*np.trunc(Ueff/hbar/omegaLaser+1)*((ellipk(Keldysh1**2)-ellipe(Keldysh1**2))/(ellipe(Keldysh2**2))))
+  
+  return IonizationRate
+
+## Conversion between field and intensity (SI units)
+# TODO: As it is absorbed field, it should multiplied by real(optical index)
+# not exactly given by Keldysh theory, but rather by :
+# 1. Value at rest (can be taken in "MaterialOpticalDatabase.dat" 
+# 2. Value with excitation using a Drude model, associated with some effective mass and collision frequency. 
+def FieldToIntensity(Field, permittivity=1):
+  intensity = 0.5 * c * epsilon_0 * real(sqrt(permittivity)) * Field**2
   return intensity
 
 ## This section can consume GB of RAM. Do not use. #{{{ 
@@ -116,4 +140,5 @@ Keldysh1 = np.vectorize(Keldysh1)
 Keldysh2 = np.vectorize(Keldysh2)
 EffectiveGap=np.vectorize(EffectiveGap)
 KeldyshFunction=np.vectorize(KeldyshFunction)
-IonizationRate=np.vectorize(IonizationRate)
+KeldyshFunction_Gruzdev=np.vectorize(KeldyshFunction_Gruzdev)
+IonizationRate_Gruzdev=np.vectorize(IonizationRate_Gruzdev)

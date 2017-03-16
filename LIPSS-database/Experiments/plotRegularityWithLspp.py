833d1a5c57d085649d5d7b3d50831bc97111655f
Thibault Derrien <derrien@fzu.cz>
2017-03-16 15:10:16 +0100

    Old modifications

--- LIPSS-database/Experiments/plotRegularityWithLspp.py
+++ LIPSS-database/Experiments/plotRegularityWithLspp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from SimpleSPProutines import *
+from libSPP import *
 #reset
 
 print "==== Plot the exp. LIPSS regularity as function of theoretical Lspp... ===="

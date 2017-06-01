5afa4850c06d6955fad76ee6b371c5018dd6dcae
Encorte <fpreucil@seznam.cz>
2017-06-02 00:15:48 +0200

    More experimenting

--- MultilayerSPP/test.py
+++ MultilayerSPP/test.py
@@ -4,55 +4,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import root, fsolve
 from scipy.linalg import norm
+from itertools import product
 
-eps1 = -1+1j
-eps2 = 2-3j
-eps3 = 1-7j
-t = 1
-k0 = 2
+eps1 = 1 + 1j
+eps2 = 3 + 1j
+eps3 = 1 + 3j
+k0 = 1
+t = 2
 
-def disp(betaR, denom):
-    betaC = betaR[0] + 1j * betaR[1]
-    k1 = cmath.sqrt(betaC*betaC - k0*k0*eps1)
-    k2 = cmath.sqrt(betaC*betaC - k0*k0*eps2)
-    k3 = cmath.sqrt(betaC*betaC - k0*k0*eps3)
+def disp(betaR):
+    betaC = betaR[0] + betaR[1]*1j
+    k1 = sgn1*cmath.sqrt(betaC*betaC - k0*k0*eps1)
+    k2 = sgn2*cmath.sqrt(betaC*betaC - k0*k0*eps2)
+    k3 = sgn3*cmath.sqrt(betaC*betaC - k0*k0*eps3)
 
-    if denom:
-        out = (k1/eps1 - k2/eps2) * (k1/eps1 - k3/eps3)
-    else:
-        out = (k1/eps1 - k2/eps2) * (k1/eps1 - k3/eps3) * cmath.exp(-2*k1*t) - (k1/eps1 + k2/eps2) * (k1/eps1 + k3/eps3)      
+    out = (k1/eps1 - k2/eps2) * (k1/eps1 - k3/eps3) * cmath.exp(-2*k1*t) - (k1/eps1 + k2/eps2) * (k1/eps1 + k3/eps3)      
     return [out.real, out.imag]
 
-def func(w):
-    z = w[0] + 1j * w[1]
-    f  = z**10 + 1
-    return [f.real, f.imag]
-
-roots = []
-
-for x in np.linspace(-1.5, 1.5, num=40):
-    for y in np.linspace(-120, 120, num=2400):
-        nrt = root(disp, [x, y], args=(False,))
-        if nrt.success:
-            nrt2 = root(disp, nrt.x, args=(False,))
-            if nrt2.success:
+mroots = []
+for sgn1, sgn2, sgn3 in product((-1,1), (-1,1), (-1,1)):
+    roots = []
+    for x in np.linspace(-2, 2, num=500):
+        for y in np.linspace(-10, 10, num=500):
+            nrt = root(disp, [x, y], method='hybr')
+            if nrt.success:
                 new = True
-                if norm(disp(nrt2.x, True)) < 1e-10:
+                if norm(disp(nrt.x)) > .01:
                     new = False
                 for rt in roots:
-                    if norm(rt - nrt2.x) < 1e-5:
+                    if norm(rt - nrt.x) < 1e-6:
                         new = False
                 if new:
-                    roots.append(nrt2.x)
-                    print('%+.9f' % nrt2.x[0], '%+.9fi' % nrt2.x[1], '      ', '%+.9f' % norm(disp(nrt2.x, False)), '%+.9f' % norm(disp(nrt2.x, True)))
+                    roots.append(nrt.x)
+                    print('%+.9f' % nrt.x[0], '%+.9fi' % nrt.x[1], '      ', '%+.9f' % norm(disp(nrt.x)), root(disp, nrt.x, method='lm').success)
+    print(len(roots))
+    mroots.append(roots)
 
-
-
-print(len(roots))
-
-plt.scatter(*zip(*roots))
+a = cmath.sqrt(eps1*k0*k0)
+plt.plot((a.real, -a.real), (a.imag, -a.imag), 'r')
+plt.scatter(*zip(*mroots[0]), c='red')
+plt.scatter(*zip(*mroots[1]), c='green')
+plt.scatter(*zip(*mroots[2]), c='blue')
+plt.scatter(*zip(*mroots[3]), c='black')
+plt.scatter(*zip(*mroots[4]), c='magenta')
+plt.scatter(*zip(*mroots[5]), c='cyan')
+plt.scatter(*zip(*mroots[6]), c='lime')
+plt.scatter(*zip(*mroots[7]), c='orangered')
 
 plt.show()
+
 ##
 ##i = 0
 ##data = []

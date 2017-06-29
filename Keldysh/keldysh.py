Keldysh: corrected a wonderful bug in the envelope computation for double-pulses

Keldysh/keldysh.py
@@ -28,38 +28,38 @@ def SiliconLDAbandGap(): #{{{
   meff=0.2226e0; #Effective mass of Si
   Ntotal=1.*5E28
 
-  wavelength = 800e-9; wavelength2 = 800e-9
+  wavelength = 800e-9;
   tau=10e-15; dt = 1E-17; CEP=0e0
   PeakFluence = 0.01*1E4 #J/cm2 * 1E4 = J/m2
   PeakField   = np.sqrt(2e0 * PeakFluence / (tau * c * epsilon_0))
 
   t0=0. #defines the instant 0.
-  Delay = 0. #delay between maxima of the pulses
+  Delay = 10e-15 #delay between maxima of the pulses
   tmin=-1.*tau + t0; tmax=1.*tau + Delay + t0
 
   instants = np.arange(tmin, tmax, dt)
   #print "Time range: "+str(instants.min())+", "+str(instants.max())+"."
 
-  PeakField2  = 0. #/ 2.
-  CEP2        = 0. #pi/3.
-  #wavelength2 = wavelength
+  PeakField2  = PeakField
+  CEP2        = pi/3.
+  wavelength2 = wavelength #/ 2.
 
   print Header+"** Test: building single pulse centered on 0..."
   FieldEnvelope1, RealField1 = PulseSquaredSinTemporalShape(instants, tau, PeakField, wavelength, CEP, t0, 0.)
 
   print Header+"** Test: We build a second pulse with a delay..."
-  FieldEnvelope2, RealField2 = PulseSquaredSinTemporalShape(instants, tau, PeakField2, wavelength2, CEP, t0, Delay)
+  FieldEnvelope2, RealField2 = PulseSquaredSinTemporalShape(instants, tau, PeakField2, wavelength2, CEP2, t0, Delay)
 
   print Header+"** Test: building a bicolor double pulse"
 
-  FieldEnvelopeTot, RealFieldTot = PulseSquaredSinTemporalShapeDoublePulse(instants, tau, tau, PeakField, PeakField, wavelength, wavelength2, CEP, CEP2, t0, Delay)
+  FieldEnvelopeTot, RealFieldTot = PulseSquaredSinTemporalShapeDoublePulse(instants, tau, tau, PeakField, PeakField2, wavelength, wavelength2, CEP, CEP2, t0, Delay)
 
-  plt.plot(instants, RealField1.real, '-')
-  plt.plot(instants, FieldEnvelope1.real, '--')
-  plt.plot(instants, RealField2.real, '-')
-  plt.plot(instants, FieldEnvelope2.real, '--')
-  plt.plot(instants, RealFieldTot.real, '-')
-  plt.plot(instants, FieldEnvelopeTot.real, '--')
+  plt.plot(instants, RealField1.real, 'r-')
+  plt.plot(instants, FieldEnvelope1.real, 'r--')
+  plt.plot(instants, RealField2.real, 'b-')
+  plt.plot(instants, FieldEnvelope2.real, 'b--')
+  plt.plot(instants, RealFieldTot.real, 'k-')
+  plt.plot(instants, FieldEnvelopeTot.real, 'k--')
   plt.xlabel('')
   plt.savefig('PulseEnvelopes.eps')
   plt.savefig('PulseEnvelopes.png')
@@ -168,4 +168,5 @@ def SilicaGulley2012(): #{{{
   return 0
 #}}}
 
-SilicaGulley2012()
+#SilicaGulley2012()
+SiliconLDAbandGap()

Keldysh/libKeldysh.py
@@ -274,7 +274,7 @@ def PulseSquaredSinTemporalShape(t, tau, PeakField, wavelength, CEP=0., t0=0., P
   H1 = step(t - t1 + tau) #! theer could be a mistake in pulse duration here!
   H2 = step(t - t1 - tau)
   Envelope = PeakField*np.sin(pi*(t-t1-tau)/(2e0*tau))**2 * H1 * (1.-H2)
-  Phase = np.exp(1e0j*omega*t+CEP)
+  Phase = np.exp(1e0j*(omega*t+CEP))
   Field = Envelope * Phase
   return Envelope, Field
 
@@ -301,10 +301,11 @@ def PulseSquaredSinTemporalShapeDoublePulse(t, tau1, tau2, Efield1, Efield2, wav
   H12        = step(t - t1 - tau1); H22 = step(t - t2 - tau2)
   FieldEnv1     = Efield1*np.sin(pi*(t-t1-tau1)/(2e0*tau1))**2 * H11 * (1.-H12) #could be bugged
   FieldEnv2     = Efield2*np.sin(pi*(t-t2-tau2)/(2e0*tau2))**2 * H21 * (1.-H22) #could be bugged
-  Phase1 = np.exp(1e0j*omega1*t+CEP1)
-  Phase2 = np.exp(1e0j*omega2*t+CEP2)
+  Phase1 = np.exp(1e0j*(omega1*t+CEP1))
+  Phase2 = np.exp(1e0j*(omega2*t+CEP2))
   #TotalEnvelope = np.sqrt( FieldEnv1*np.conj(FieldEnv1) + FieldEnv2*np.conj(FieldEnv2) + FieldEnv1*np.conj(FieldEnv2) * np.exp(1e0j*(omega1-omega2)*t) + np.conj(FieldEnv1)* FieldEnv2 * np.exp(1e0j*(omega2-omega1)*t) ) #complex square of the fields must provide the envelope
-  TotalEnvelope = np.sqrt( FieldEnv1*np.conj(FieldEnv1) * np.exp(2.*CEP1) + FieldEnv2*np.conj(FieldEnv2) * np.exp(2.*CEP2) + FieldEnv1*np.conj(FieldEnv2) * np.exp(1e0j*(omega1-omega2)*t+CEP1+CEP2) + np.conj(FieldEnv1)* FieldEnv2 * np.exp(1e0j*(omega2-omega1)*t+CEP1+CEP2) ) #complex square of the fields must provide the envelope
+  #BUG: the phase does not work properly! Rederive the following formula! 
+  TotalEnvelope = np.sqrt( FieldEnv1*np.conj(FieldEnv1) + FieldEnv2*np.conj(FieldEnv2) + FieldEnv1*np.conj(FieldEnv2) * np.exp(1e0j*((omega1-omega2)*t+CEP1-CEP2)) + np.conj(FieldEnv1)* FieldEnv2 * np.exp(1e0j*((omega2-omega1)*t+CEP2-CEP1)) ) #complex square of the fields must provide the envelope
   TotalField = FieldEnv1*Phase1 + FieldEnv2*Phase2
   return TotalEnvelope, TotalField
 
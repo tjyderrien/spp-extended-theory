047a79db5f0996dddd6b92216d72650662397cfc
Thibault Derrien <derrien@fzu.cz>
2017-07-30 19:41:24 +0900

    Keldysh: modeled the pulse from S. Gräf et al 2017.

--- Keldysh/keldysh.py
+++ Keldysh/keldysh.py
@@ -34,7 +34,7 @@ def SiliconLDAbandGap(): #{{{
   PeakField   = np.sqrt(2e0 * PeakFluence / (tau * c * epsilon_0))
 
   t0=0. #defines the instant 0.
-  Delay = 10e-15 #delay between maxima of the pulses
+  Delay = 20e-15 #delay between maxima of the pulses
   tmin=-1.*tau + t0; tmax=1.*tau + Delay + t0
 
   instants = np.arange(tmin, tmax, dt)
@@ -42,7 +42,7 @@ def SiliconLDAbandGap(): #{{{
 
   PeakField2  = PeakField
   CEP2        = pi/3.
-  wavelength2 = wavelength #/ 2.
+  wavelength2 = wavelength / 2.
 
   print Header+"** Test: building single pulse centered on 0..."
   FieldEnvelope1, RealField1 = PulseSquaredSinTemporalShape(instants, tau, PeakField, wavelength, CEP, t0, 0.)
@@ -61,8 +61,9 @@ def SiliconLDAbandGap(): #{{{
   plt.plot(instants, RealFieldTot.real, 'k-')
   plt.plot(instants, FieldEnvelopeTot.real, 'k--')
   plt.xlabel('')
-  plt.savefig('PulseEnvelopes.eps')
-  plt.savefig('PulseEnvelopes.png')
+  filename = 'PulseEnvelopes-'+str(wavelength*1E9)+'nm-'+str(wavelength2*1E9)+'nm-CEP2'+str(CEP/pi * 180.)+'+delay'+str(Delay*1E15)+'fs'
+  plt.savefig(filename+'.eps')
+  plt.savefig(filename+'.png')
   #plt.show()
 
   print Header+"** Info: PulseEnvelope.EPS and PNG were written in the current folder. "
@@ -83,7 +84,7 @@ def SiliconLDAbandGap(): #{{{
   plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, dt, 40, ShowPlot)
   plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, dt, 50, ShowPlot)
 
-  print Header+"** Test 2: computing the W_PI values from self-coded and validated Gruzdev theory..."
+  print Header+"** Test 2: computing the W_PI values from self-coded and Gruzdev theory..."
   timeKeldysh, N_Keldysh_SI, N_Gruzdev_SI = plotPulseToDensity(Egap, meff, wavelength, tau, FieldEnvelope.real, dt, order, ShowPlot, 0e0, Ntotal)
 
   print Header+"** Test 3: computing the W_PI values from Vladimir Zhukov tables..."
@@ -168,5 +169,85 @@ def SilicaGulley2012(): #{{{
   return 0
 #}}}
 
+# Repeats the results obtained in Gulley, Opt. Eng. 51, 121805 (2012). 
+def SilicaGraef2017(): #{{{
+  print "Defining SiO2 material parameters from [Gräf2017]..."
+
+  Egap = 9e0*e; #band gap of SiO2
+  meff = 1e0; #Effective mass of SiO2
+  Ntotal=10.*5E28; #valence band electron density #to avoid limitation
+
+  wavelength = 1030e-9; wavelength2 = 800e-9
+  tau=300e-15; dt = 1E-17; CEP=0e0
+  PeakFluence = 5.*5E4
+  PeakField   = np.sqrt(2e0 * PeakFluence / (tau * c * epsilon_0))
+
+  t0=0. #defines the instant 0.
+  Delay = 0. #delay between maxima of the pulses
+  tmin=-1.*tau + t0; tmax=1.*tau + Delay + t0
+
+  instants = np.arange(tmin, tmax, dt)
+  #print "Time range: "+str(instants.min())+", "+str(instants.max())+"."
+
+  PeakField2  = 0. #/ 2.
+  CEP2        = 0. #pi/3.
+  #wavelength2 = wavelength
+  
+  print Header+"** Test: building single pulse centered on 0..."
+  FieldEnvelope1, RealField1 = PulseSquaredSinTemporalShape(instants, tau, PeakField, wavelength, CEP, t0, 0.)
+
+  print Header+"** Test: We build a second pulse with a delay..."
+  FieldEnvelope2, RealField2 = PulseSquaredSinTemporalShape(instants, tau, PeakField2, wavelength2, CEP, t0, Delay)
+
+  print Header+"** Test: building a bicolor double pulse"
+
+  FieldEnvelopeTot, RealFieldTot = PulseSquaredSinTemporalShapeDoublePulse(instants, tau, tau, PeakField, PeakField, wavelength, wavelength2, CEP, CEP2, t0, Delay)
+
+  plt.plot(instants, RealField1.real, '-')
+  plt.plot(instants, FieldEnvelope1.real, '--')
+  plt.plot(instants, RealField2.real, '-')
+  plt.plot(instants, FieldEnvelope2.real, '--')
+  plt.plot(instants, RealFieldTot.real, '-')
+  plt.plot(instants, FieldEnvelopeTot.real, '--')
+  plt.xlabel('')
+  plt.savefig('PulseEnvelopes.eps')
+  plt.savefig('PulseEnvelopes.png')
+  #plt.show()
+
+  print Header+"** Info: PulseEnvelope.EPS and PNG were written in the current folder. "
+
+  order = 50
+  ShowPlot = True
+
+  print Header+"** Test 1: computing the W_PI values from self-coded and validated Gruzdev theory..."
+  timeKeldysh, N_Keldysh_SI, N_Gruzdev_SI = plotPulseToDensity(Egap, meff, wavelength, tau, FieldEnvelope1.real, dt, order, ShowPlot, 0e0, Ntotal)
+  
+  #print Header+"** Test 0: Convergence test using the Keldysh-Gruzdev formulas..."
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, 1E-17, order, ShowPlot)
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, 5E-17, order, ShowPlot)
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, 1E-16, order, ShowPlot)
+  
+  #print "Checking dt convergence..."
+  #print ""
+  #print "Checking order convergence..."
+  
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, dt, 10, ShowPlot)
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, dt, 20, ShowPlot)
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, dt, 30, ShowPlot)
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, dt, 40, ShowPlot)
+  #plotPulseToDensity(Egap, meff, wavelength, tau, PeakFluence, dt, 50, ShowPlot)
+
+  #print Header+"** Test 1: computing the W_PI values from self-coded and validated Gruzdev theory..."
+  #timeKeldysh, N_Keldysh_SI, N_Gruzdev_SI = plotPulseToDensity(Egap, meff, wavelength, tau, FieldEnvelope1.real, dt, order, ShowPlot, 0e0, Ntotal)
+
+  #print Header+"** Test 2: computing the W_PI values from Vladimir Zhukov tables..."
+  #print Header+"           WE DONT HAVE THEM FOR THIS BAND GAP. Contact zukov@ict.nsc.ru."
+  #wPI_Zhukov = VZ_generateWpiTables(FieldEnvelope1, FieldEnvelope2, wavelength, wavelength2, CEP, CEP2, Egap, meff, tau, tau, Delay, dt, Ntotal, t0)
+  
+  return 0
+#}}}
+
+
 #SilicaGulley2012()
-SiliconLDAbandGap()
+#SiliconLDAbandGap()
+SilicaGraef2017()

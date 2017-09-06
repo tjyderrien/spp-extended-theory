83662cc853a45095822fd2cf7aeac4a9d42043d4
Encorte <fpreucil@seznam.cz>
2017-09-07 00:32:45 +0200

    Added comments

--- Keldysh/libKeldyshMcDonald.py
+++ Keldysh/libKeldyshMcDonald.py
@@ -2,32 +2,38 @@ import math, cmath
 from scipy.integrate import ode
 import matplotlib.pyplot as plt
 
+#some constants
 T_2 = 1
 F_0 = 1
 d_0x = 1
 
+#external functions
 def f(t):
     return math.sin(t)
 def S(t):
-    return t
+    return 1
 
+#initial condition, etc...
 t_start = 0.0
 t_stop = 10.0
 dt = .05
-X_start = [1.0, 2.0j, -1.0j]
+X_start = [1.0, 2.0, 3.0]
 
-def odeRHS(t, X):
+#contains the ODE system matrix
+def ODE_matrix(t, X):
     pi = X[0]
-    nv = X[1]
-    nc = X[2]
-    pi_dot = -pi/T_2 - 1j*F_0*f(t)*d_0x*(nv - nc)*cmath.exp(-1j*S(t))
-    nv_dot = 2*F_0*f(t)*d_0x*(pi*cmath.exp(1j*S(t))).imag
-    nc_dot = -nv_dot
-    return [pi_dot, nv_dot, nc_dot]
+    n_v = X[1]
+    n_c = X[2]
+    pi_dot = -pi/T_2 - 1j*F_0*f(t)*d_0x*(n_v - n_c)*cmath.exp(-1j*S(t))
+    n_v_dot = 2*F_0*f(t)*d_0x*(pi*cmath.exp(1j*S(t))).imag
+    n_c_dot = -n_v_dot
+    return [pi_dot, n_v_dot, n_c_dot]
 
-r = ode(odeRHS).set_integrator('zvode', method='bdf')
+#initializing the integrator
+r = ode(ODE_matrix).set_integrator('zvode', method='bdf')
 r.set_initial_value(X_start, t_start)
 
+#integrate the problem to get results
 while r.successful() and r.t < t_stop:
     r.integrate(r.t + dt)
     print(r.t, r.y)

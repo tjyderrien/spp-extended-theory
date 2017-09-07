418c31487c1e69317da6ac0b629a75612ca84986
Encorte <fpreucil@seznam.cz>
2017-09-07 15:01:58 +0200

    Update

--- Keldysh/libKeldyshMcDonald.py
+++ Keldysh/libKeldyshMcDonald.py
@@ -3,45 +3,56 @@ from scipy.integrate import ode
 import matplotlib.pyplot as plt
 
 #some constants
-T_2 = 1
 F_0 = 1
 d_0x = 1
+omega_0 = 1
+t_0 = 5
+T_2 = 1
+log2 = math.log(2)
 
 #external functions
-def f(t):
-    return math.sin(t)
+def Omega(t):
+    return F_0*d_0x*math.cos(omega_0*t)*math.exp(-log2*((t - t_0)/(5*omega_0))**2)
 def S(t):
-    return 1
+    return t
 
 #initial condition, etc...
-t_start = 0.0
-t_stop = 10.0
-dt = .05
-X_start = [1.0, 2.0, 3.0]
+t_start = 0
+t_stop = 10
+dt = .1
+X_start = [2, 2, 1]
 
 #contains the ODE system matrix
 def ODE_matrix(t, X):
     pi = X[0]
     n_v = X[1]
     n_c = X[2]
-    pi_dot = -pi/T_2 - 1j*F_0*f(t)*d_0x*(n_v - n_c)*cmath.exp(-1j*S(t))
-    n_v_dot = 2*F_0*f(t)*d_0x*(pi*cmath.exp(1j*S(t))).imag
+    pi_dot = -pi/T_2 - 1j*Omega(t)*(n_v - n_c)*cmath.exp(-1j*S(t))
+    n_v_dot = 2*Omega(t)*(pi*cmath.exp(1j*S(t))).imag
     n_c_dot = -n_v_dot
     return [pi_dot, n_v_dot, n_c_dot]
 
-#initializing the integrator
+#initializes the integrator
 r = ode(ODE_matrix).set_integrator('zvode', method='bdf')
 r.set_initial_value(X_start, t_start)
 
-#integrate the problem to get results
+#integrates the problem to get results
+times = []
+pi_Re = []
+pi_Im = []
+n_v = []
+n_c = []
 while r.successful() and r.t < t_stop:
-    r.integrate(r.t + dt)
     print(r.t, r.y)
-    plt.scatter(r.t, r.y[0].real, c='r')
-    plt.scatter(r.t, r.y[0].imag, c='g')
-    plt.scatter(r.t, r.y[1].real, c='b')
-    plt.scatter(r.t, r.y[1].imag, c='k')
-    plt.scatter(r.t, r.y[2].real, c='cyan')
-    plt.scatter(r.t, r.y[2].imag, c='magenta')
+    times.append(r.t)
+    pi_Re.append(r.y[0].real)
+    pi_Im.append(r.y[0].imag)
+    n_v.append(r.y[1].real)
+    n_c.append(r.y[2].real)
+    r.integrate(r.t + dt)
 
+plt.plot(times, pi_Re, 'r')
+plt.plot(times, pi_Im, 'r', linestyle='--')
+plt.plot(times, n_v, 'g')
+plt.plot(times, n_c, 'b')
 plt.show()

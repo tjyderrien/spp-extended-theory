ece3a541025a5919168baa2f67d3a2cf1a3e56c9
Encorte <fpreucil@seznam.cz>
2017-09-27 22:46:46 +0200

    Tried an analytic formula for the vector potential

--- Keldysh/libKeldyshMcDonald.py
+++ Keldysh/libKeldyshMcDonald.py
@@ -2,10 +2,14 @@ import math, cmath
 import numpy as np
 from scipy.integrate import ode
 from scipy.integrate import quad
+from scipy.special import erf #for analytic
+from scipy.special import dawsn
 import matplotlib.pyplot as plt
 
 #some constants
 log2 = math.log(2)
+slog2 = math.sqrt(math.log(2))
+spilog2 = math.sqrt(math.pi/math.log(2))
 band_freq = (5.32, 6.14, 9.83)                                          #band frequencies
 band_amps_v = ((-0.0928, 0.0705, 0.0200, -0.0012, 0.0029, 0.0006),      #band amplitudes
                (-0.0307, 0.0307, 0, 0, 0, 0),
@@ -13,23 +17,26 @@ band_amps_v = ((-0.0928, 0.0705, 0.0200, -0.0012, 0.0029, 0.0006),      #band am
 band_amps_c = ((0.0898, -0.0814, -0.0024, -0.0048, -0.0003, -0.0009),
                (0.1147, -0.1147, 0, 0, 0, 0),
                (0.0435, -0.0435, 0, 0, 0, 0))
-E_g = 3.3                                                               #gap energy
+E_g = 0                                                                 #gap energy
 d_0x = 3.46                                                             #x-component of the dipole moment
 
 F_0 = 1                                                                 #laser field parameters
 omega_0 = 2
-t_0 = 20
+t_0 = 30
 
 T_2 = 1                                                                 #what is this?
+
+#some precached numbers
+prefactor = -5/2*F_0*omega_0*spilog2*cmath.exp(-25*omega_0*omega_0*omega_0*omega_0/(4*log2)-1j*t_0*omega_0)
+a_anl = slog2/(5*omega_0)
+b_anl = (-t_0*2*log2+25j*omega_0*omega_0*omega_0)/(10*omega_0*slog2)
 
 #dispersion curve
 def Epsilon(k):
     total = E_g
     for i in range(0, 3):       #iterating over directions
-        total_in_dir = 0
         for j in range(0, 6):   #cosine sum
-            total_in_dir += (band_amps_c[i][j]-band_amps_v[i][j])*math.cos(j*k[i]*band_freq[i])/3
-        total += total_in_dir
+            total += (band_amps_c[i][j]-band_amps_v[i][j])*math.cos(j*k[i]*band_freq[i])/3
     return total
 
 #we only have 1D field
@@ -38,64 +45,83 @@ def F_x(t):
     return F_0*math.cos(omega_0*t)*math.exp(-log2*((t-t_0)/(5*omega_0))**2)
 #x-component of the vector potential, temporal integral of the laser field
 def A_x(t):
-    return quad(F_x, -np.inf, t, limit=100)[0]
+    return analytic_A_x(t)
+    #return quad(F_x, -np.inf, t, limit=500)[0]
+
+#analytic vector potential
+def analytic_A_x(t):
+    return (prefactor*(1-erf(a_anl*t+b_anl))).real
 
 #action integral
 def S(K, t):
-    return quad(lambda t, K: Epsilon([K[0] + A_x(t), K[1], K[2]]), -np.inf, t, args=K, limit=100)[0]
+    return quad(lambda t, K: Epsilon([K[0]+A_x(t), K[1], K[2]]), -np.inf, t, args=K, limit=500)[0]
 
+#--------------------------------------------------------------------------------------------------
+
+K = [0, 0, 0]
 #initial condition
-t_start = -10
-t_stop = 18
-samples = 600
+t_init = 0
+t_end = 40
+samples = 400
 
-pi_start = 0
-n_v_start = 2
-n_c_start = 1
-S_start = 0
-A_start = 0
+pi_init = 0
+n_v_init = 2
+n_c_init = 1
+S_init = S(K, t_init)
+
+print('t_init = %f' % t_init)
+print('t_end = %f' % t_end)
+print('S_init = %.15f' % S_init)
 
 #contains the ODE system RHS
 def RHS(t, X):
     pi = X[0]
     n_v = X[1]
     n_c = X[2]
+    #A_x = X[3]
     S = X[3]
-    A = X[4]
     pi_dot = -pi/T_2-1j*d_0x*F_x(t)*(n_v-n_c)*cmath.exp(-1j*S)
     n_v_dot = 2*d_0x*F_x(t)*(pi*cmath.exp(1j*S)).imag
     n_c_dot = -n_v_dot
-    S_dot = Epsilon([A.real, 0, 0])
-    A_dot = F_x(t)
-    return [pi_dot, n_v_dot, n_c_dot, S_dot, A_dot]
+    #A_x_dot = F_x(t)
+    S_dot = Epsilon([K[0]+A_x(t), K[1], K[2]])
+    return [pi_dot, n_v_dot, n_c_dot, S_dot]
 
 #initializes the integrator
 #https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.ode.html
-r = ode(RHS).set_integrator('zvode', method='bdf', nsteps=5000)
+intg = ode(RHS).set_integrator('zvode', method='bdf', nsteps=5000)
 
 #integrates the problem to get results
-times = np.linspace(t_start, t_stop, samples)
+times = np.linspace(t_init, t_end, samples)
 pi_Re = []
 pi_Im = []
 n_v = []
 n_c = []
 Ss = []
-As = []
+A_xs = []
+F_xs = []
+
+tlen = len(times)
+ind = 1
 for t in times:
-    r.set_initial_value([pi_start, n_v_start, n_c_start, S_start, A_start], t_start)
-    r.integrate(t)
-    pi_Re.append(r.y[0].real)
-    pi_Im.append(r.y[0].imag)
-    n_v.append(r.y[1].real)
-    n_c.append(r.y[2].real)
-    Ss.append(r.y[3].real)
-    As.append(r.y[4].real)
+    intg.set_initial_value([pi_init, n_v_init, n_c_init, S_init], t_init)
+    intg.integrate(t)
+    pi_Re.append(intg.y[0].real)
+    pi_Im.append(intg.y[0].imag)
+    n_v.append(intg.y[1].real)
+    n_c.append(intg.y[2].real)
+    A_xs.append(A_x(t))
+    Ss.append(intg.y[3].real)
+    F_xs.append(F_x(t))
+    print('Progress: %d%%' % (ind/tlen * 100), end='\r')
+    ind += 1
 
 plt.plot(times, pi_Re, 'r', label='Re(pi)')
 plt.plot(times, pi_Im, 'r', linestyle='--', label='Im(pi)')
 plt.plot(times, n_v, 'g', label='n_v')
 plt.plot(times, n_c, 'b', label='n_c')
-#plt.plot(times, Ss, 'yellow', label='S')
-plt.plot(times, As, 'magenta', label='A')
+plt.plot(times, A_xs, 'm', label='A_x')
+plt.plot(times, Ss, 'c', label='S')
+plt.plot(times, F_xs, 'gray', label='F_x')
 plt.legend(loc='upper right')
 plt.show()

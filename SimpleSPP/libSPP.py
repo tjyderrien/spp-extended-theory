3425eb51e05c0fc726464ddcf918298e6766818d
Thibault Derrien <derrien@fzu.cz>
2018-01-12 11:33:51 +0100

    Modified tolerance over libSPP.py

--- SimpleSPP/libSPP.py
+++ SimpleSPP/libSPP.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #-*- coding: utf-8 -*-
 
 # Copyright (C) 2013-2017 T. J.-Y. Derrien
@@ -43,14 +43,14 @@ from libMaterials import *
 from libMath import *
 
 lengthunit = 1e-9
-eta = 0.5 #assumed precision error on the dielectric permittivity
+eta = 0.1 #assumed precision error on the dielectric permittivity
 UsingTeX=True #TODO: set to False for Windows users
 
 ## 0: all permisive, no verification on SPP excitation condition
 ## 1: use the RegularLIPSScondition, softer than pure SPP excitation condition
 ## 2: Period != 0 is necessary for a material to be listed in results
 ## 3: Extreme level: use ExperimentallyAchievable() to verify possibility of decay depth > optical penetration depth
-LevelOfSPPaccuracy=2
+LevelOfSPPaccuracy=1
 
 # Settings for matplotlib: taken from https://stackoverflow.com/questions/12322738/how-do-i-change-the-axis-tick-font-in-a-matplotlib-plot-when-rendering-using-lat
 sizeOfFont = 18

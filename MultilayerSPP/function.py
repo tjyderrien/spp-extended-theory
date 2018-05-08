e1ca8b9f5a43df0c664181dafb2287ae00a693ab
Thibault Derrien <thibault.derrien@mpsd.mpg.de>
2018-05-08 21:35:11 +0200

    Successfully tested the functionalized solver for multilayer SPP. We can now use the routine extensively.

--- MultilayerSPP/function.py
+++ MultilayerSPP/function.py
@@ -118,15 +118,15 @@ def findroots(eps1, eps2, eps3, wavelength, t, x_min, x_max, y_min, y_max, x_ste
 
 ##        print()
         branches.append(unique)
-
+  # Selection of the maximum Lspp
     outs = []
     for branch in branches:
         maxy = 0
         maxx = 0
         for rt in branch:
-            xi = 1E6*2.*np.pi/rt[0]
-            yi = 1E9*.5/rt[1]
-            if yi > maxy:
+            xi = 2.*np.pi/rt[0]
+            yi = .5/rt[1]
+            if yi > maxy: #select the branches with absolute maximum Lspp
                 maxy = yi
                 maxx = xi
         outs.append([maxx, maxy])
@@ -140,14 +140,54 @@ def findroots(eps1, eps2, eps3, wavelength, t, x_min, x_max, y_min, y_max, x_ste
 #             wavelength, t,
 #             x_min, x_max,       }
 #             y_min, y_max,       } mesh parameters
-#             x_steps, y_steps}   }
+#             x_steps, y_steps)   }
 #
 
 #Example:
-roots = findroots(-1+1j, 1, 1,
-                  1, 1,
-                  -1E10, 1E10,
-                  -1E9, 1E9,
-                  30, 30)
+#roots = findroots(-1+1j, 1, 1,
+                  #1, 1,
+                  #-1E10, 1E10,
+                  #-1E9, 1E9,
+                  #30, 30)
 
+# === PRODUCTION OF SCIENTIFIC RESULTS ===
+
+#data
+wavelength = 1026e-9 #355e-9 #1030E-9 #1026
+epsTibare   = -6.206969+25.2j #800 nm
+epsTiO2bare = 7.7841+0.j      #800 nm
+epsCr       = -0.672122310000001+24.8657476j #-0.67+24.87j    #1026 nm
+epsBK7      = 2.10277365777   #1026 nm
+epsCr2O3    = 4.9713+0.1784j  #1 um [JDT Kruschwitz et al, Appl. Opt. 1997]
+epsSi       = 12.8159503769+0.0114635303918j #1026 nm, Palik
+epsAir      = 1.+0.j          #air
+#epsCu       = -46.6046581932 + 4.7188669976j #1030 nm
+epsCu       = -1.9937293241+4.9290716854j     #355  nm
+
+# Medium 1: thin film. 
+eps1 = epsCr        #thin film
+# Medium 2: substrate. 
+eps2 = epsBK7       #epsBK7 #environment | substrate
+# Medium 3: environment
+eps3 = epsAir       #environment | substrate
+# Note: Inverting eps2 and eps3 should have no effect on the possible modes, but only on field amplification. 
+
+t = 100E-9 #thickness of the layer in meters
+
+#meshes the initial guess area, all numbers are from the space of betas
+x_min = -1E10
+x_max = 1E10
+
+y_min = -1E9
+y_max = 1E9
+
+x_steps = 40
+y_steps = 40
+
+roots = findroots(eps1, eps2, eps3,
+          wavelength, t,
+          x_min, x_max,    
+          y_min, y_max,    
+          x_steps, y_steps)
+print("SPP modes: [Period, Lspp]")
 print(roots)

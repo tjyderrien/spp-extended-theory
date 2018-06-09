c0062932e58ae94ec141474fc29de21db11ecdd4
Thibault Derrien <thibault.derrien@mpsd.mpg.de>
2018-06-10 01:04:02 +0200

    MultilayerSPP: validation on Derrien and Bonse JAP 2014 has been added to libMultilayerSPP.

--- MultilayerSPP/plotDerrien2014.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Produces the plot submitted to MetaNano conference proceeding 2018 with Prof. Bulgakova and A. Dostovalov. 
-
-set -e
-./derrien2014.py &> Dostovalov-PeriodWithThickness.csv
-gnuplot plotThicknessPeriod.gnu --persist

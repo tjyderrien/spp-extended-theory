d1bf8615ad560bf81f000aa5c0307c50f33a5734
Thibault Derrien <thibault.derrien@mpsd.mpg.de>
2018-06-10 18:40:18 +0200

    MultilayerSPP: the case of Dostovalov is ready (laser induced oxidation scenario).

--- MultilayerSPP/dostovalov.py
+++ MultilayerSPP/dostovalov.py
@@ -92,13 +92,16 @@ def ScenarioOfOxidePrecipitation():
         for branch in np.arange(0,num_branches-1):
             print("1.", thickness, roots[branch][0], roots[branch][1], eps1.real, eps1.imag)
 
-# Scenario proposed by Nadya
+## Scenario proposed by Nadya
+# Pulse by pulse, oxygen from ambient air diffuses into the Cr, and leads to formation of Cr2O3. 
+# By making use of a Lorenz-Lorentz model, one can construct a dielectric permittivity of the oxide
+# and therefore observe the transition between SPP and waveguiding modes. 
 def ScenarioOfCrOxideMixture():
     print(Header, "# Info: Considering a mixed fraction of Cr with Cr2O3 with several thicknesses.")
-
-    fraction_size = 20
-    fraction_min = 0.5
-    fraction_max = 1.0
+    wavelength = 1026e-9
+    fraction_size = 60
+    fraction_min = 0.
+    fraction_max = 1.
 
     thickness_size = 1
     thickness_min  = 28e-9
@@ -106,7 +109,7 @@ def ScenarioOfCrOxideMixture():
 
     ## Running 
 
-    fraction = np.arange(fraction_min, fraction_max, (fraction_max-fraction_min)/float(fraction_size)) #fraction of Cr
+    fraction = np.linspace(fraction_min, fraction_max, fraction_size) #fraction of Cr
     epsCrCr2O3_list = MaxwellGarnett2(epsCr, epsCr2O3, 1.-fraction)
     print(Header, "# Info: size of the fraction matrix: ", fraction_size)
 
@@ -153,6 +156,13 @@ def ScenarioOfCrOxideMixture():
                     if(roots[branch][root_number][0] != 0e0): 
                         summary = np.vstack((summary, ToBeAdded ))
 
+    ExperimentalData_velocity   = np.array([1e-6, 10e-16, 50e-6, 100e-6, 200e-6, 300e-6]) #m/s
+    ExperimentalData_LSFL       = np.array([696e-9, 704e-9, 816e-9, 858e-9, -100e0, -100e-9]) #m #better observed for low velocities, i.e., high number of pulses, i.e., largest amounts of oxide
+    ExperimentalData_LSFL_error = np.array([78e-9,71e-9,139e-9,140e-9,0e-9,0e-9]) #m
+    ExperimentalData_HSFL       = np.array([170e-9, 159e-9, 217e-9, 244e-9, 249e-9, 238e-9]) #m
+    ExperimentalData_HSFL_error = np.array([64e-9,38e-9,101e-9,110e-9,128e-9,72.5e-9]) #m
+    CrFraction_Fitted           = np.array([0.6e0, 0.7e0, 0.8e0, 0.86e0, 0.90e0, 0.95e0]) #hand fitted to match period with existing modes [on request of Nadya]
+
 #== Extract the constructed table
     thickness     = summary[:, 0] 
     branch        = summary[:, 1]
@@ -172,18 +182,64 @@ def ScenarioOfCrOxideMixture():
     #ImBeta = np.divide(0.5,lspp_s)
 
     plt.figure()
-    ax1 = plt.subplot(111)
-    plt.xlabel(r'Fraction of Cr$_2$O$_3$ (perc.)')
-    plt.ylabel('SPP period (m)')
-    ax12 = ax1.twinx()
-    plt.ylabel(r'SPP mean free path $L_{SPP}$ (m)')
+    ax1 = plt.subplot(211)
+    plt.title(r'Film thickness $t=$'+str(round(np.real(thickness[0])*1e9))+' nm')
+    #plt.xlabel(r'Fraction of Cr (perc.)')
+    plt.ylabel(r'SPP period $\Lambda$ (nm)') 
+    #ax1y = ax1.twiny()
+    plot1y1 = ax1.errorbar(CrFraction_Fitted, 1e9*ExperimentalData_LSFL, yerr=1e9*ExperimentalData_LSFL_error, fmt='ro', label=r'Period LSFL')
+    plot1y2 = ax1.errorbar(CrFraction_Fitted, 1e9*ExperimentalData_HSFL, yerr=1e9*ExperimentalData_HSFL_error, fmt='r^', label=r'Period HSFL')
+    #ax1.set_yscale('log')
+    plt.ylim((0.,1.1e9*wavelength))
+    #ax12 = ax1.twinx()
+    #plt.ylabel(r'SPP mean free path $L_{SPP}$ (m)')
+    plt.ylabel(r'Re($\varepsilon$), Im($\varepsilon$)')
     # Then we could plot them in the right order
-    plot11, = ax1.loglog(fractionOxide, period_s, 'r+', label=r'Period $\Lambda$')
-    plot12, = ax1.loglog(fractionOxide, wavelength*np.ones(np.shape(thickness_s)), '-', label=r'$\lambda$')
-    plot13, = ax12.loglog(fractionOxide, lspp_s, 'b+', label=r'$L_{SPP}$')
+    plot11, = ax1.plot(fractionOxide_s, np.multiply(1e9,period_s), 'r+', label=r'Period $\Lambda$')
+    plot12, = ax1.plot(fractionOxide_s, np.multiply(1e9,wavelength*np.ones(np.shape(thickness_s))), 'k--', label=r'$\lambda$')
+    
+    #plot14, = ax12.plot(fractionOxide_s, np.real(epsilonFilm_s), 'b+', label=r'Re($\varepsilon$)')
+    #plot15, = ax12.plot(fractionOxide_s, np.imag(epsilonFilm_s), 'b^', label=r'Im($\varepsilon$)')
+    
+    ax1.yaxis.label.set_color(plot11.get_color()) #colorizes the label
+    ax1.spines["left"].set_edgecolor(plot11.get_color()) #colorizes the axis
+    ax1.tick_params(axis='y', colors=plot11.get_color()) #colorizes the tics and numbers
+    
+    #ax12.yaxis.label.set_color(plot14.get_color()) #colorizes the label
+    #ax12.spines["right"].set_edgecolor(plot14.get_color()) #colorizes the axis
+    #ax12.tick_params(axis='y', colors=plot14.get_color()) #colorizes the tics and numbers
+    
     plt.tight_layout()
-
-    plt.legend(loc='best')
+    
+    #plot1   = [plot11, plot12, plot14, plot15]
+    #labels1 = [l.get_label() for l in plot1]
+    #ax1.legend(plot1, labels1, loc='best')
+    
+    ax2 = plt.subplot(212)
+    plt.ylabel(r'$L_{SPP}$ decay length (m)')
+    plt.xlabel(r'Fraction of Cr (perc.)')
+    plot21, = ax2.semilogy(fractionOxide_s, lspp_s,   'r^', label=r'SPP decay length $L_{SPP}$')
+    ax22 = ax2.twinx()    
+    plt.ylabel(r'Re($\varepsilon$), Im($\varepsilon$)')
+    plot22, = ax22.plot(fractionOxide_s, np.real(epsilonFilm_s), 'b+', label=r'Re($\varepsilon$)')
+    plot23, = ax22.plot(fractionOxide_s, np.imag(epsilonFilm_s), 'b^', label=r'Im($\varepsilon$)')
+    plot24, = ax22.plot(fractionOxide_s, np.multiply(epsBK7, np.ones(np.shape(fractionOxide_s))), 'b--', label=r'$Re[\varepsilon$(BK7)] ')
+    
+    ax2.yaxis.label.set_color(plot21.get_color()) #colorizes the label
+    ax2.spines["left"].set_edgecolor(plot21.get_color()) #colorizes the axis
+    ax2.tick_params(axis='y', colors=plot21.get_color()) #colorizes the tics and numbers
+    
+    ax22.yaxis.label.set_color(plot22.get_color()) #colorizes the label
+    ax22.spines["right"].set_edgecolor(plot22.get_color()) #colorizes the axis
+    ax22.tick_params(axis='y', colors=plot22.get_color()) #colorizes the tics and numbers
+    
+    plot2   = [plot21, plot22, plot23, plot24]
+    plotComb= [plot11, plot12, plot21, plot22, plot23, plot24]
+    #labels2 = [l.get_label() for l in plot2]
+    labelsComb = [l.get_label() for l in plotComb]
+    #ax2.legend(plot2, labels2, loc='best')
+    ax2.legend(plotComb, labelsComb, loc='best')
+    
     plt.tight_layout()
     plt.savefig("Dostovalov_Cr2O3mixedWithCr_Thickness.eps")
     plt.show()
@@ -194,4 +250,5 @@ def ScenarioOfCrOxideMixture():
 # ** Info: computing 3-layer reflectivity..."
 #R = BiLayerReflectivity(epsAir, epsCrCr2O3_list, epsBK7, t_list) #dimension is good for a HeatMap picture
 
-ScenarioOfOxidePrecipitation()
+#ScenarioOfOxidePrecipitation()
+ScenarioOfCrOxideMixture()

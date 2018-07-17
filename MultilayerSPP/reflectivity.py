9afd3343318edb0c83bbe4245f2c12ca0f292bf2
Thibault Derrien <thibault.derrien@mpsd.mpg.de>
2018-07-17 17:10:18 +0200

    Computed Krystof Mo R(thickness case)

--- MultilayerSPP/reflectivity.py
+++ MultilayerSPP/reflectivity.py
@@ -24,12 +24,27 @@ import matplotlib.pyplot as plt
 ## EXAMPLE OF USAGE at 800 nm
 wavelength=1064e-9
 
-eps1=1.;
+epsAir=1.;
 if(wavelength == 1064e-9): 
-    eps2=-14.083065233570098+20.789041764340013j; eps3=1.4496**2
+    epsMo=-14.083065233570098+20.789041764340013j; epsSiO2=1.4496**2; epsSLG = 2.2889 + 0.000014899j
 elif(wavelength == 800e-9): 
-    eps2=2.08+24.52j; eps3=1.4533**2
+    epsMo=2.08+24.52j; epsSiO2=1.4533**2; epsSLG = 2.3018 + 0.0000075160j
+
+# Medium 1: thin film. 
+eps2 = epsMo        #thin film
+# Medium 2: substrate. 
+eps3 = epsSLG # | epsSiO2       #epsBK7 #environment | substrate
+# Medium 3: environment
+eps1 = epsAir       #environment | substrate
+# Note: Inverting eps2 and eps3 should have no effect on the possible modes, but only on field amplification. 
+
+
+#if(wavelength == 1064e-9): 
+    #eps2=-14.083065233570098+20.789041764340013j; eps3=1.4496**2
+#elif(wavelength == 800e-9): 
+    #eps2=2.08+24.52j; eps3=1.4533**2
 thickness2 = np.arange(1e-9, 200e-9, 1e-9)
+
 R = BiLayerReflectivity(wavelength, eps1, eps2, eps3, thickness2)
 
 filename = "Mo-"+str(int(1E9*wavelength))+"-Reflectivity"

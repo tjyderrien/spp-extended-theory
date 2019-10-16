c0cafff01738c139f3e7e4da85ac4ac52d0fb594
Thibault JY Derrien <derrien@fzu.cz>
2019-10-16 17:07:42 +0200

    libDatabase: added possibility to introduce a str for header.

--- SimpleSPP/libDatabase.py
+++ SimpleSPP/libDatabase.py
@@ -132,10 +132,10 @@ def ExtractDataDb(SPPdbFiltered):
 ## Export an SPP array to a CSV file
 # SPP array must be produced with one of the SPPactiveInterfaces functions
 #
-def ExportToTxt(dbarray, filename):
+def ExportToTxt(dbarray, filename, header=""):
   
   try: 
-    np.savetxt(filename, dbarray, fmt="%s", delimiter='\t', newline='\n',comments='#')
+    np.savetxt(filename, dbarray, fmt="%s", delimiter='\t', header=header, newline='\n',comments='#')
     out = 0
   except: 
     print "Could not write database into a file."

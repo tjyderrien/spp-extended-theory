b28a5083d2cb5a645f04c31324e06d30a5a00e8b
Thibault JY Derrien <derrien@fzu.cz>
2020-03-20 18:11:59 +0100

    MultilayerSPP: reflectivity.py: added Ti, Mo and corrected the index of media.

--- MultilayerSPP/reflectivity.py
+++ MultilayerSPP/reflectivity.py
@@ -22,7 +22,7 @@ from libMaterials import *
 import matplotlib.pyplot as plt
 
 ## EXAMPLE OF USAGE 
-wavelength=1030e-9
+wavelength=1064e-9
 
 ## TODO: replace this by a function taking data in MaterialOpticalData.csv !
 epsAir=1.;
@@ -33,13 +33,18 @@ elif(wavelength == 800e-9):
 elif(wavelength==1030e-9):
     epsSi   = 12.80259+0.0109j
     epsSiO2 = 2.1026565205
+    epsTi   = -4.2656+27.277j
+    epsMo   = -11.6291789477+20.6107572133j
+elif(wavelength==400e-9):
+    epsMo=-1.1887745095+19.5149912779j; 
+    epsSiO2=2.1614446988; #epsSLG = 2.2889 + 0.000014899j
 
-# Medium 1: thin film. 
-eps2 = epsSiO2        #thin film
-# Medium 2: substrate. 
-eps3 = epsSi # | epsSiO2       #epsBK7 #environment | substrate
-# Medium 3: environment
-eps1 = epsAir       #environment | substrate
+# Medium 1: External medium. 
+eps1 = epsAir        #thin film
+# Medium 2: Thin film. 
+eps2 = epsMo # | epsSiO2       #epsBK7 #environment | substrate
+# Medium 3: Substrate
+eps3 = epsSLG       #environment | substrate
 # Note: Inverting eps2 and eps3 should have no effect on the possible modes, but only on field amplification. 
 
 
@@ -47,17 +52,21 @@ eps1 = epsAir       #environment | substrate
     #eps2=-14.083065233570098+20.789041764340013j; eps3=1.4496**2
 #elif(wavelength == 800e-9): 
     #eps2=2.08+24.52j; eps3=1.4533**2
-thickness_log = np.linspace(-9, np.log10(500e-9), 200)
+thickness_log = np.linspace(-9, np.log10(250e-9), 1000)
 thickness2 = np.power(10., thickness_log)
 
 R = BiLayerReflectivity(wavelength, eps1, eps2, eps3, thickness2)
-
-filename = "SiO2-Si-"+str(int(1E9*wavelength))+"-Reflectivity"
+T = BiLayerTransmission(wavelength, eps1, eps2, eps3, thickness2)
+A = 1.-R-T
+filename = "Air-Si-Air-"+str(int(1E9*wavelength))+"-Reflectivity"
 
 plt.figure()
 plt.xlabel("Film thickness (nm)")
 plt.ylabel("Reflectivity")
-plt.plot(1E9*thickness2, R)
+plt.plot(1E9*thickness2, R, label="R")
+plt.plot(1E9*thickness2, T, label="T")
+plt.plot(1E9*thickness2, A, label="A")
+plt.legend(loc="best")
 plt.grid()
 plt.savefig(filename+".eps")
 plt.show()

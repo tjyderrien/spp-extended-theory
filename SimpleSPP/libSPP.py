74bc5354eaf65876fb98eb6983865e0cfcd83d0f
Krystof Hlinomaz <khlinomaz@fzu.cz>
2021-02-19 17:04:43 +0100

    spp-extended-theory ported from python2 to python3 automatically with 2to3 tool. Comments are not changed !

--- SimpleSPP/libSPP.py
+++ SimpleSPP/libSPP.py
@@ -470,10 +470,10 @@ def SPPactiveInterfaces(dbarray, comment):#{{{
         
         # Print only the experimentally possible cases: SPP active depth must be smaller than absorption depth. 
         # ensure that SPPdecayDepth is smaller than layer thickness, to avoid shift of dispersion relation
-        if(LevelOfSPPaccuracy < 3): 
-		ExperimentalAchievable = True #ExperimentallyAchievable(OpticalPenetration1, SPPdecayDepth1)
+        if(LevelOfSPPaccuracy < 3):
+          ExperimentalAchievable = True #ExperimentallyAchievable(OpticalPenetration1, SPPdecayDepth1)
         else:
-		ExperimentalAchievable = ExperimentallyAchievable(OpticalPenetration1, SPPdecayDepth1)
+          ExperimentalAchievable = ExperimentallyAchievable(OpticalPenetration1, SPPdecayDepth1)
         
         # Print the table of active SPP interfaces for all cases or only new SPP interfaces					
         #if( not (comment=="new")):

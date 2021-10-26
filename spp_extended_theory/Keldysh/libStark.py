c09e22162393c8b9f2bc04780f6ea8d94b3a8c10
Thibault J.-Y. Derrien <derrien@fzu.cz>
2021-10-26 16:43:52 +0200

    libStark.py: double integration was not necessary. Single was necessary.

--- spp_extended_theory/Keldysh/libStark.py
+++ spp_extended_theory/Keldysh/libStark.py
@@ -385,12 +385,13 @@ def ComputeFloquetBandStructure(filename, nb_atoms, Z_electrons, kpoints, unocc_
             for t in np.arange(tmin,tmax,dt):
                 integral1=Efield_AU**2*np.cos(omega_AU*t)**2*dt
                 integral1_sum=np.add(integral1_sum, integral1)
-            for t in np.arange(tmin,tmax,dt):
-                integral2=integral1_sum*dt
-                integral2_sum=np.add(integral2_sum, integral2)
+#           for t in np.arange(tmin,tmax,dt):
+#                integral2=integral1_sum*dt
+#                integral2_sum=np.add(integral2_sum, integral2)
         else:
-            integral2_sum=0
-        return np.add(np.add(H_GS, Efield_AU*np.cos(omega_AU*t)*matrixelements), integral2_sum)
+            integral1_sum=0
+#            integral2_sum=0
+        return np.add(np.add(H_GS, Efield_AU*np.cos(omega_AU*t)*matrixelements), integral1_sum)
         
     ## Returns the Rabi frequency for each possible dipolar transition (atomic units)
     # WARNING: 1/c error may be found (when employed convention is E=-1/c dA/dt). 

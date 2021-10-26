8ec6567bf4b44ab89154f44eb6b0b9ff48ca45b9
Kristyna Gazdova <gazdova.kristyna.muni@gmail.com>
2021-10-26 18:12:06 +0200

    libStark: artificially checking the effect of A2

--- spp_extended_theory/Keldysh/libStark.py
+++ spp_extended_theory/Keldysh/libStark.py
@@ -387,6 +387,7 @@ def ComputeFloquetBandStructure(filename, nb_atoms, Z_electrons, kpoints, unocc_
         integral1_sum=0
         integral2_sum=0
         if Enable_A2:
+            integral1_sum=(np.add(H_GS, Efield_AU*np.cos(0.95*omega_AU*t)*matrixelements))
             for t in np.arange(tmin,tmax,dt):
                 integral1=Efield_AU**2*np.cos(omega_AU*t)**2*dt
                 integral1_sum=np.add(integral1_sum, integral1)

libStark.py: Line 395: for case with A2, I changed integration variable from t to tprime. The A2 loop was replacing t, turning band structure into something possibly wrong. Analytical calculation in the article should address this point. Moreover, long duration of A2 term computation is mostly due to double integration in time. Number of timesteps is the critical parameter for reducing the integration time.

spp_extended_theory/Keldysh/libStark.py
@@ -390,8 +390,8 @@ def ComputeFloquetBandStructure(filename, nb_atoms, Z_electrons, kpoints, unocc_
 #        assert (Enable_A2)
         if Enable_A2:
 #            integral1_sum=(np.add(H_GS, Efield_AU*np.cos(0.95*omega_AU*t)*matrixelements))
-            for t in np.arange(tmin,tmax,dt):
-                integral1=Efield_AU**2*np.cos(omega_AU*t)**2*dt
+            for tprime in np.arange(tmin,tmax,dt):
+                integral1=Efield_AU**2*np.cos(omega_AU*tprime)**2*dt
                 integral1_sum=np.add(integral1_sum, integral1)
 #           for t in np.arange(tmin,tmax,dt):
 #                integral2=integral1_sum*dt